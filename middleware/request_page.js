9e33c4a00f2ea17cf5fb61a081c84aa56f4f29ca
tungsten-cat <uly4nkin.denis@gmail.com>
2023-04-12 21:19:26 +0300

    Added some coments

--- middleware/request_page.js
+++ middleware/request_page.js
@@ -1,9 +1,12 @@
-
+// Including required modules for this script
 const consola = require("consola");
 
+// This function will describe our middleware
 module.exports.middleware = (request, response, handler) =>
 {
+    // Logging requested page address
     consola.info(`Requested page: ${request.path}`);
 
+    // This call required to continue execution process
     handler();
 }
